89af676fd400cfbd77b99e674e76faed70efff54
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-04-25 09:23:38 +0600

    html almost complete

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".review-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
